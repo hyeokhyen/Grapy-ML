eebec92c8bba2c4bc64065a450f3a38d279741ad
hyeok kwon <hyeokhyen@gatech.edu>
2021-03-12 14:01:54 -0500

    change path

--- dataloaders/mypath_pascal.py
+++ dataloaders/mypath_pascal.py
@@ -2,7 +2,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(database):
         if database == 'pascal':
-            return './data/datasets/pascal/'  # folder that contains pascal/.
+            return '/nethome/hkwon64/Research/imuTube/repos_v2/human_parsing/Grapy-ML/data/datasets/pascal/'  # folder that contains pascal/.
         else:
             print('Database {} not available.'.format(database))
             raise NotImplementedError
